37c19d7ccc754585b9105a2a3068e4c1addb2462
Jan Fechner <jan.fechner@inosoft.com>
2019-09-05 16:51:40 +0200

    .

---
+++ YetAnotherXmppClient/Core/StanzaParts/DiscoInfoFeature.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+
+namespace YetAnotherXmppClient.Core.StanzaParts
+{
+    class DiscoInfoFeature : XElement
+    {
+        public DiscoInfoFeature(string name)
+            : base(XNames.discoinfo_feature, new XAttribute("var", name))
+        {
+
+        }
+    }
+}
---
+++ YetAnotherXmppClient/Core/StanzaParts/DiscoInfoIdentity.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+
+namespace YetAnotherXmppClient.Core.StanzaParts
+{
+    class DiscoInfoIdentity : XElement
+    {
+        public DiscoInfoIdentity(string category, string type, string name)
+            : base(XNames.discoinfo_identity, new XAttribute("category", category), new XAttribute("type", type), new XAttribute("name", name))
+        {
+
+        }
+    }
+}
