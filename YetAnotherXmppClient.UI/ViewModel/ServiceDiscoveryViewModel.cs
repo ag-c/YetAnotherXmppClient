60299817864639e4133f811611ea5afdeb449461
Jan Fechner <jan.fechner@inosoft.com>
2019-09-23 08:00:30 +0200

    renaming

--- YetAnotherXmppClient.UI/ViewModel/ServiceDiscoveryViewModel.cs
+++ YetAnotherXmppClient.UI/ViewModel/ServiceDiscoveryViewModel.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Reactive;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using ReactiveUI;
 using YetAnotherXmppClient.Infrastructure;
 using YetAnotherXmppClient.Infrastructure.Queries;
-using YetAnotherXmppClient.Protocol.Handler;
 using YetAnotherXmppClient.Protocol.Handler.ServiceDiscovery;
 
 namespace YetAnotherXmppClient.UI.ViewModel
 {
-    //public class EntityViewModel
-    //{
-    //    private ObservableCollection<EntityViewModel> Entities { get; set; } = new ObservableCollection<EntityViewModel>();
-    //}
-
     public class ServiceDiscoveryViewModel : ReactiveObject
     {
         private readonly IMediator mediator;
@@ -27,10 +17,10 @@ namespace YetAnotherXmppClient.UI.ViewModel
         public ServiceDiscoveryViewModel(IMediator mediator, string jid)
         {
             this.mediator = mediator;
-            this.OnRefreshAsync(jid);
+            this.RefreshAsync(jid);
         }
 
-        private async Task OnRefreshAsync(string jid)
+        private async Task RefreshAsync(string jid)
         {
             var info = await this.mediator.QueryAsync<QueryEntityInformationTreeQuery, EntityInfo>(new QueryEntityInformationTreeQuery { Jid = jid});
             this.RootEntityInfo.Clear();
