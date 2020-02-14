c9d1e9fc48bf6530c3c73906a8a393e1b905c2d1
Jan Fechner <jan.fechner@inosoft.com>
2020-02-14 14:34:34 +0100

    began work on multi user chat protocol handler

---
+++ YetAnotherXmppClient/Protocol/Handler/MultiUserChatProtocolHandler.cs
@@ -0,0 +1,78 @@
+﻿
+
+// XEP-0045: Multi-User Chat
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YetAnotherXmppClient.Core;
+using YetAnotherXmppClient.Infrastructure;
+using YetAnotherXmppClient.Infrastructure.Queries;
+using YetAnotherXmppClient.Protocol.Handler.ServiceDiscovery;
+
+namespace YetAnotherXmppClient.Protocol.Handler.MultiUserChat
+{
+    internal enum Role
+    {
+        None,
+        Moderator,
+        Participant,
+        Visitor
+    }
+
+    internal enum Affiliation
+    {
+        None,
+        Owner,
+        Admin,
+        Member,
+        Outcast
+    }
+
+    internal enum RoomType
+    {
+        Hidden,
+        MembersOnly,
+        Moderated,
+        NonAnonymous,
+        Open,
+        PasswordProtected,
+        Persistent,
+        Public,
+        SemiAnonymous,
+        Temporary,
+        Unmoderated,
+        Unsecured
+    }
+
+    public class Room
+    {
+        public string Jid { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal class MultiUserChatProtocolHandler : ProtocolHandlerBase
+    {
+        public MultiUserChatProtocolHandler(XmppStream xmppStream, Dictionary<string, string> runtimeParameters, IMediator mediator)
+            : base(xmppStream, runtimeParameters, mediator)
+        {
+        }
+
+        public async Task<IEnumerable<Room>> DiscoverRoomsAsync(string jid)
+        {
+            var supportsMuc = await this.Mediator.QueryAsync<EntitySupportsFeatureQuery, bool>(new EntitySupportsFeatureQuery(jid, ProtocolNamespaces.MultiUserChat)).ConfigureAwait(false);
+            if (!supportsMuc)
+            {
+                return null;
+            }
+
+            var items = await this.Mediator.QueryAsync<EntityItemsQuery, IEnumerable<Item>>(new EntityItemsQuery(jid)).ConfigureAwait(false);
+
+            return items.Select(itm => new Room
+                                           {
+                                               Jid = itm.Jid,
+                                               Name = itm.Name
+                                           });
+        }
+    }
+}
